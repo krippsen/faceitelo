889fe4396e7016f3f2023229308faac1a8f412b4
Petr Kristen <73830760+krippsen@users.noreply.github.com>
2024-04-09 00:39:48 +0200

    Update elo.js
    cs2 lvl?

--- elo.js
+++ elo.js
@@ -14,7 +14,7 @@ function updateelo(){
   oReq.send();
   oReq.onload = function() {
     elo = JSON.parse(oReq.responseText).games.cs2.faceit_elo;
-    levelpic.src = "https://cdn-frontend.faceit.com/web/960/src/app/assets/images-compress/skill-icons/skill_level_"+ JSON.parse(oReq.responseText).games.csgo.skill_level + "_svg.svg";
+    levelpic.src = "https://cdn-frontend.faceit.com/web/960/src/app/assets/images-compress/skill-icons/skill_level_"+ JSON.parse(oReq.responseText).games.cs2.skill_level + "_svg.svg";
     elodisplay.innerText = elo;
   }
 }
